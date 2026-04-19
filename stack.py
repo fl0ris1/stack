d70fc2bfe7cfdeca714b8ede95d4e681c014e9a4
Floris <149056628+fl0ris1@users.noreply.github.com>
2026-04-19 10:59:17 +0200

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,36 @@
+class Stack:
+    def __init__(self,n):
+        self.stack=[]
+        self.maxSize=n #maximum number of elements in the stack
+        
+    def push(self,val):
+        if len(self.stack)<self.maxSize:
+            self.stack.append(val)
+        else:
+            print("STACK FULL")
+            
+    def pop(self):
+        if len(self.stack)>0:
+            return self.stack.pop()
+        else:
+            print("NO VALUES AVAILIBLE")
+            
+    def top(self):
+        if len(self.stack)>0:
+            return self.stack[len(self.stack)-1]
+        else:
+            print("NO VALUES AVAILIBLE")
+            
+    def size(self):
+        return len(self.stack)
+    
+    def isEmpty(self):
+        if len(self.stack)==0:
+            return True
+        return False
+    
+    def display(self):
+        print(self.stack)
+        
+        
+stack=Stack(5)
