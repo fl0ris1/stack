548b1d0578dfab6c09326db3f787ce6deaf5b545
Floris <149056628+fl0ris1@users.noreply.github.com>
2026-04-26 10:53:31 +0200

    Add files via upload

--- stack_reverse.py
+++ stack_reverse.py
@@ -17,9 +17,9 @@ class Stack():
             
     def display(self):
         print(self.stack)
+        
 
 inp=input("Input A Word: ")
-
 stack=Stack(len(inp))
 
 for i in range(len(inp)-1,-1,-1):
